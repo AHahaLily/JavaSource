4998b15b209bf6448562c33b235384ce5e741352
SulliSun <mmail_tel@163.com>
2019-12-04 20:59:17 +0800

    Create Demo02.java

---
+++ src/day02/Demo02.java
@@ -0,0 +1,21 @@
+package day02;
+
+public class Demo02 {
+
+	public static void main(String[] args) {
+		//关系运算符> < >= <= == !=
+		//关系运算符的返回值类型是能是boolean类型
+		int a=10;
+		int b=5;
+		boolean flag1=a>b;
+		boolean flag2=a<=b;
+		boolean flag3=a==b;
+		boolean flag4=a!=b;
+		System.out.println(flag1);  //true
+		System.out.println(flag2);  //false
+		System.out.println(flag3);  //false
+		System.out.println(flag4);  //true
+		
+	}
+
+}
