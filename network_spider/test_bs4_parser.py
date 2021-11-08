f836c65d0b2a4db026a245369293c7f03d2f373d
Zoey <v-cazheng@microsoft.com>
2021-11-08 18:03:06 +0800

    update test_bs4_parser.py

--- network_spider/test_bs4_parser.py
+++ network_spider/test_bs4_parser.py
@@ -14,3 +14,25 @@ soup=BeautifulSoup(html_doc,'lxml')
 
 # prettify()方法格式化输出html/xml文档
 print(soup.prettify())
+
+# 获取p标签的html代码
+print(soup.p)
+
+# 获取b标签代码
+print(soup.p.b)
+
+# 获取p标签内容
+print(soup.p.text)
+
+# 返回属性和值组成的字典
+print(soup.p.attrs)
+
+# 返回数据类型
+print(type(soup.p))
+
+# 根据属性返回属性值，返回值为列表
+print(soup.p['class'])
+
+# 给class属性赋值，属性值由列表转换为字符串
+soup.p['class']=['Web','Site']
+print(soup.p)
